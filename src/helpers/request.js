83795757197c86681c842e215793f740c1599114
Lucas Zampronio <lucas.zampronio.silva@gmail.com>
2025-06-05 20:49:47 -0300

    Test

---
+++ src/helpers/request.js
@@ -0,0 +1,23 @@
+const request = require('supertest');
+
+const baseURL =  "https://fakestoreapi.com";
+
+async function getRequest(path){
+    try {
+
+        const response = await request(baseURL)
+        .get(path)
+        .set('Accept','application/json')
+        .expect('Content-Type','application/json; charset=utf-8')
+        .expect(200);
+        return response.body
+
+    } catch(error){
+        console.error(error);
+        throw error;
+    }
+}
+
+module.exports = {
+    getRequest
+}
